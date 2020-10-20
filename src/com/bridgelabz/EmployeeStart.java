c4d1cd67a8db4bd9493a14eb122a9f034e3e4632
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-20 19:05:49 +0530

    added use switch

--- src/com/bridgelabz/EmployeeStart.java
+++ src/com/bridgelabz/EmployeeStart.java
@@ -1,11 +1,14 @@
 package com.bridgelabz;
 public class EmployeeStart 
+
+
+
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("******Welocme to Employee Wages Problem*******");
+		System.out.println("Welocme to Employee Wages Problem");
 		
-		int Is_Present=1;
+		
 		int Salary=0;
 		int Is_Part_Time=0;
 		int Is_Full_Time=1;
@@ -14,31 +17,27 @@ public class EmployeeStart
 		int Part_Time_Hr=4;
 		
 //Genrating Random Number
-		int EmpCheck = (int) (Math.floor(Math.random() *10 % 2));
-		int EmpCheck1 = (int) (Math.floor(Math.random() *10 % 2));
-//Checking Employee And Caluclating Wages
-		if (EmpCheck == Is_Present)
+		int EmpCheck = (int) (Math.floor(Math.random() *10 % 3));
+//Checking Employee And Caluclating Wages Using Case
+		
+		switch(EmpCheck)
 		{
-			if (EmpCheck1 == Is_Full_Time)
-			{
+			case 1:
 				Salary=EmpHrs*EmpRate_Hour;
 				System.out.println("Employee Is Present and Salary is: " +Salary);
-				
-			}
-							
-			else
-			{
+			break;
+			
+			case 2:
 				Is_Part_Time=EmpRate_Hour*Part_Time_Hr;
 				System.out.println("Employee Is Present and Salary is: " +Is_Part_Time);
-					
-			} //Else Block Over
+			break;
+			
+			default:
+				System.out.println("Employee is Absent and Salary is: " +Salary);
+			break;
+			
+		}	//Switch Case
 		
-		}
-		else
-		{
-			System.out.println("Employee Is Absent & Salary is :" +Salary);
-		}
+	} //Public
 
-	} //Public ends
-
-}// MAin Class ends
+}// MAin Class
