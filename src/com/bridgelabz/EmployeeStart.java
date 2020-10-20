9f408128ca264c089dbb70f063e3c4be4dafa130
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-20 18:21:12 +0530

    added: employee present or not

--- src/com/bridgelabz/EmployeeStart.java
+++ src/com/bridgelabz/EmployeeStart.java
@@ -1,10 +1,24 @@
 package com.bridgelabz;
 
-public class EmployeeStart {
+
+
+public class EmployeeStart{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("welcome to empyolee wages computation");
-	}
+			System.out.println("welcome to employee wages");
+		int IS_FULL_TIME = 1;
+		double empCheck = (Math.floor(Math.random() *10)%2);
+		if (empCheck == IS_FULL_TIME) {
+			
+		
+		System.out.println("employee is present");
 
+	}
+		else
+		{
+			System.out.println("not present");
+		}
+
+}
 }
