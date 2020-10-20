68a562510fc11584f226f66e50c9882ac6e87081
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-20 18:38:29 +0530

    added:employee daily wages

--- src/com/bridgelabz/EmployeeStart.java
+++ src/com/bridgelabz/EmployeeStart.java
@@ -2,23 +2,40 @@ package com.bridgelabz;
 
 
 
-public class EmployeeStart{
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			System.out.println("welcome to employee wages");
-		int IS_FULL_TIME = 1;
-		double empCheck = (Math.floor(Math.random() *10)%2);
-		if (empCheck == IS_FULL_TIME) {
-			
-		
-		System.out.println("employee is present");
+public class EmployeeStart {
 
-	}
-		else
-		{
-			System.out.println("not present");
+	
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				
+					System.out.println("welcome to employee wages");
+					
+				int IS_FULL_TIME = 1;
+				int WAGE_PER_HOUR=20;
+				int DAILY_EMPLOY_WAGES=0;
+				int i;
+				int num=0;
+				Scanner sc= new Scanner(System.in);
+				System.out.printf("enter the number");
+				DAILY_EMPLOY_WAGES=sc.nextInt();
+				for(i=0;i<=num;i++)
+				{
+				double empCheck = (Math.floor(Math.random() *10)%2);
+				System.out.println(empCheck);
+				if (empCheck == IS_FULL_TIME) {
+					
+					DAILY_EMPLOY_WAGES=WAGE_PER_HOUR*8; 
+					System.out.println(DAILY_EMPLOY_WAGES);
+				System.out.printf("employees daily wages: "+DAILY_EMPLOY_WAGES);
+
+			}
+				else
+				{
+					System.out.println("EMPLOYEE not  PRESENT");
+				}
+
 		}
-
-}
-}
+				}
+		}
