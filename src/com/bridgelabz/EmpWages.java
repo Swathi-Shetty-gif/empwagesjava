0a68c891ccea701b68e86fd5805af9f485f04a3a
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-21 06:49:10 +0530

    added Emp employee wages

---
+++ src/com/bridgelabz/EmpWages.java
@@ -0,0 +1,27 @@
+package com.bridgelabz;
+
+public class EmpWages {
+
+	
+
+		public static void main(String[] args) {
+		int fullTime=1;
+		int partTime=2;
+		int Wages_Per_Hour=20;
+		int full_Day_Hour=8;
+		int Part_Day_Hour=4;
+		int days=20;
+		int checkout=(int) (Math.floor(Math.random() *10) %3);
+		System.out.println(checkout);
+		switch(checkout){
+			case 1 :int total=Wages_Per_Hour * full_Day_Hour;
+								System.out.println(total * days);
+								break;
+			case 2 :int total1=Wages_Per_Hour * Part_Day_Hour;
+								System.out.println(total1 * days);
+								break;
+			default : System.out.println("0");
+		}
+							
+		}
+		}
